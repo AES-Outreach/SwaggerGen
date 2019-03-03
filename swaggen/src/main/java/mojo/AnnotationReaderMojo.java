7873c31be9aeb6e6d78eec86f9cb78056f03c0a8
alexseguin <segualex@gmail.com>
2019-03-03 11:59:28 -0500

    clean

--- swaggen/src/main/java/mojo/AnnotationReaderMojo.java
+++ swaggen/src/main/java/mojo/AnnotationReaderMojo.java
@@ -2,12 +2,15 @@ package mojo;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -22,10 +25,6 @@ import org.reflections.util.ConfigurationBuilder;
 
 import annotation.SwaggerGen;
 
-/**
- * TODO - Add function call to annotation reader
- *
- */
 @Mojo(name = "process-annotations", requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class AnnotationReaderMojo extends AbstractMojo {
 
@@ -33,41 +32,76 @@ public class AnnotationReaderMojo extends AbstractMojo {
 	private MavenProject project;
 
 	/**
-	 * The greeting to display.
+	 * Function automatically called during the install phase of a project using
+	 * SwaggerGen as a dependency. Will search the Maven project's classpath for
+	 * classes annotated with the SwaggerGen and trigger the rest of the annotation
+	 * processing.
+	 * 
+	 * @see org.apache.maven.plugin.Mojo#execute()
 	 */
-	@Parameter(property = "process-annotations.baseUrl", defaultValue = "localhost")
-	private String baseUrl;
-
-	@SuppressWarnings("unchecked")
 	public void execute() throws MojoExecutionException {
 
 		try {
 
-			Set<URL> urls = new HashSet<>();
-			List<String> elements = project.getRuntimeClasspathElements();
-			for (String element : elements) {
-				urls.add(new File(element).toURI().toURL());
-			}
+			getLog().info("-- OBTAINING ANNOTATIONS --");
+			Class<?>[] klasses = getClassArray();
 
-			ClassLoader contextClassLoader = URLClassLoader.newInstance(urls.toArray(new URL[0]),
-					Thread.currentThread().getContextClassLoader());
+			getLog().info("-- PROCESSING ANNOTATIONS --");
+			/* TRIGGER ANNOTATION PROCESSING HERE */
 
-			Thread.currentThread().setContextClassLoader(contextClassLoader);
-
-			Reflections ref = new Reflections(
-					new ConfigurationBuilder().addUrls(ClasspathHelper.forClassLoader(contextClassLoader))
-							.setScanners(new SubTypesScanner(false), new MethodAnnotationsScanner()));
-
-			getLog().info("trying to get annotations");
-			Set<Method> m = ref.getMethodsAnnotatedWith(SwaggerGen.class);
-			for (Method mm : m) {
-				getLog().info(mm.getAnnotation(SwaggerGen.class).toString());
-			}
 		} catch (Exception e) {
+			// Can add other catches here for more complete error handling
 			getLog().error(e.toString());
-			throw new MojoExecutionException("Dependency resolution failed", e);
+			throw new MojoExecutionException("Could not process annotations from project.", e);
 		}
 
 		getLog().info("-- DONE --");
+	}
+
+	/**
+	 * Uses reflections to obtain the classes that have the SwaggerGen annotation in
+	 * the importing Maven project.
+	 * 
+	 * @return array of classes that contain methods annotated with SwaggerGen
+	 * @throws DependencyResolutionRequiredException if mojo is misconfigured
+	 * @throws MalformedURLException
+	 */
+	private Class<?>[] getClassArray() throws MalformedURLException, DependencyResolutionRequiredException {
+
+		Reflections ref = new Reflections(
+				new ConfigurationBuilder().addUrls(ClasspathHelper.forClassLoader(getClassLoader()))
+						.setScanners(new SubTypesScanner(false), new MethodAnnotationsScanner()));
+
+		Set<Method> methods = ref.getMethodsAnnotatedWith(SwaggerGen.class);
+		List<Class<?>> k = new ArrayList<Class<?>>();
+		for (Method m : methods) {
+			if (!k.contains(m.getClass()))
+				k.add(m.getClass());
+		}
+
+		return (Class<?>[]) k.toArray();
+	}
+
+	/**
+	 * Gets the classloader for the maven project and loads it into the current
+	 * thread.
+	 * 
+	 * @return the classloader
+	 * @throws DependencyResolutionRequiredException if mojo is misconfigured
+	 * @throws MalformedURLException                 if URLs in the classpath are
+	 *                                               malformed
+	 */
+	private ClassLoader getClassLoader() throws DependencyResolutionRequiredException, MalformedURLException {
+		Set<URL> urls = new HashSet<>();
+		List<String> elements = project.getRuntimeClasspathElements();
+		for (String element : elements) {
+			urls.add(new File(element).toURI().toURL());
+		}
+
+		ClassLoader contextClassLoader = URLClassLoader.newInstance(urls.toArray(new URL[0]),
+				Thread.currentThread().getContextClassLoader());
+
+		Thread.currentThread().setContextClassLoader(contextClassLoader);
+		return contextClassLoader;
 	}
 }
