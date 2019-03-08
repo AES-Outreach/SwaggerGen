c5900b7f18cb9b6d47a75c300937bd7e97a8859a
Danny Petitclerc <dpetitclerc25@gmail.com>
2019-03-08 03:42:36 -0500

    Added connector to JsonObjectMapper

--- swaggen/src/main/java/utils/Main.java
+++ swaggen/src/main/java/utils/Main.java
@@ -1,6 +1,10 @@
 package utils;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import domain.path.Path;
 
@@ -12,16 +16,21 @@ import domain.path.Path;
 
 public class Main {
 
-	public static void exec(Class<?>[] klasses) {
+	public static void exec(Class<?>[] klasses) throws JsonParseException, JsonMappingException, IOException {
 		
 		/**
 		 * Given an array of classes that contain methods annotated with SwaggerGen,
 		 * generate a list of associated paths and POJO
 		 * 
 		 * @param klasses an array of classes that contain methods annotated with SwaggerGen
+		 * @throws JsonParseException
+		 * @throws JsonMappingException
+		 * @throws IOException
 		 **/
 		
 		List<Path> paths = generator.PathGenerator.generatePathsFromClassList(klasses);
-		
+		for(Class<?> k: klasses) {
+			utils.JsonObjectMapper.fileToClass(k.getName(), k);
+		}
 	}
 }
