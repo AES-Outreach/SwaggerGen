fcfb8099ed0b10742056c1d2bf19d8699bb8a320
alexseguin <segualex@gmail.com>
2019-04-05 01:11:12 -0400

    fixed issues

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,13 +1,9 @@
 # generate API docs for SwaggerGen
-cd swaggen/generated/swagger
 for x in swaggen/generated/swagger/*.yaml; do 
 	redoc-cli bundle $x;
 	t=$(echo $x | sed 's/\.yaml$/.html/'); 
 	mv redoc-static.html $t;
 done
-
-# go back to project root
-cd ../../../
 
 # generate API docs for sample-project
 for x in sample-project/generated/swagger/*.yaml; do 
