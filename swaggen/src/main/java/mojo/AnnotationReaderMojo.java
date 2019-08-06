50ea924c64f89cef4bd957958a32d45b1cdbde56
Billzzhang <billzzhang2@gmail.com>
2019-08-06 15:59:18 -0400

    Disable logging from the pom

--- swaggen/src/main/java/mojo/AnnotationReaderMojo.java
+++ swaggen/src/main/java/mojo/AnnotationReaderMojo.java
@@ -9,11 +9,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import java.util.Collection;
 
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
@@ -24,13 +22,9 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.scanners.MethodParameterNamesScanner;
-import org.reflections.scanners.MethodParameterScanner;
 import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
-import org.reflections.scanners.ResourcesScanner;
 
 import annotation.SwaggerGen;
 
@@ -49,6 +43,9 @@ public class AnnotationReaderMojo extends AbstractMojo {
     @Parameter(readonly = true)
     private String propertiesPath;
 
+    @Parameter(readonly = true)
+    private boolean showLogs;
+
     /**
      * Function automatically called during the install phase of a project using
      * SwaggerGen as a dependency. Will search the Maven project's classpath for
@@ -58,42 +55,28 @@ public class AnnotationReaderMojo extends AbstractMojo {
      * @see org.apache.maven.plugin.Mojo#execute()
      */
     public void execute() throws MojoExecutionException {
-        boolean showLogs = false;
         try {
             try (InputStream input = new FileInputStream(propertiesPath)) {
                 Properties config = new Properties();
                 config.load(input);
-                showLogs = Boolean.parseBoolean(config.getProperty("showLogs"));
-
-                if (showLogs) {
-                    getLog().info("Loaded properties file.");
-                    getLog().info("-- -- -- OBTAINING ANNOTATIONS -- -- --");
-                }
+                infoLog("Loaded properties file.");
+                infoLog("-- -- -- OBTAINING ANNOTATIONS -- -- --");
                 ClassLoader originalClassLoader = getClass().getClassLoader();
-                Class<?>[] klasses = getClassArray(showLogs);
-                if (showLogs) {
-                    getLog().info("-- -- -- PROCESSING ANNOTATIONS -- -- --");
-                    getLog().info("Swagger version number: " + config.getProperty("version"));
-                }
+                Class<?>[] klasses = getClassArray();
+                infoLog("-- -- -- PROCESSING ANNOTATIONS -- -- --");
+                infoLog("Swagger version number: " + config.getProperty("version"));
                 generator.SwaggerGenerator.generateSwaggerFile(klasses, config);
                 Thread.currentThread().setContextClassLoader(originalClassLoader);
             } catch (IOException ex) {
-                if (showLogs) {
-                    getLog().warn(
-                            "No valid properties file provided. Please add a <propertiesPath> tag to the plugin's build configuration tag.");
-                }
+                warnLog("No valid properties file provided. Please add a <propertiesPath> tag to the plugin's build configuration tag.");
                 throw new MojoExecutionException("File not found.");
             }
         } catch (Exception e) {
             // Can add other catches here for more complete error handling
-            if (showLogs) {
-                getLog().error(e.toString());
-            }
+            errorLog(e.toString());
             throw new MojoExecutionException("Could not process annotations from project.", e);
         }
-        if (showLogs) {
-            getLog().info("-- DONE --");
-        }
+        infoLog("-- DONE --");
     }
 
     /**
@@ -104,20 +87,16 @@ public class AnnotationReaderMojo extends AbstractMojo {
      * @throws DependencyResolutionRequiredException if mojo is misconfigured
      * @throws MalformedURLException
      */
-    private Class<?>[] getClassArray(boolean showLogs)
+    private Class<?>[] getClassArray()
             throws MalformedURLException, DependencyResolutionRequiredException {
 
         Reflections loadedKlasses = new Reflections(
                 new ConfigurationBuilder().addUrls(ClasspathHelper.forClassLoader(getClassLoader(showLogs)))
                         .setScanners(new SubTypesScanner(false), new MethodAnnotationsScanner()));
-        if (showLogs) {
-            getLog().info("Loaded Klasses instantiated");
-            getLog().info("About to find methods annotated with SwaggerGen using reflections object");
-        }
+        infoLog("Loaded Klasses instantiated");
+        infoLog("About to find methods annotated with SwaggerGen using reflections object");
         Set<Method> swaggenMethods = loadedKlasses.getMethodsAnnotatedWith(SwaggerGen.class);
-        if (showLogs) {
-            getLog().info("Swaggen Methods instantiated");
-        }
+        infoLog("Swaggen Methods instantiated");
         Set<Class<?>> klasses = new HashSet<Class<?>>();
         for (Method method : swaggenMethods) {
             klasses.add(method.getDeclaringClass());
@@ -134,28 +113,39 @@ public class AnnotationReaderMojo extends AbstractMojo {
      * @throws MalformedURLException                 if URLs in the classpath are
      *                                               malformed
      */
-    private ClassLoader getClassLoader(boolean showLogs) throws DependencyResolutionRequiredException, MalformedURLException {
+    private ClassLoader getClassLoader(boolean showLogs)
+            throws DependencyResolutionRequiredException, MalformedURLException {
         Set<URL> urls = new HashSet<>();
         List<String> elements = project.getRuntimeClasspathElements();
         for (String element : elements) {
             urls.add(new File(element).toURI().toURL());
-            if (showLogs) {
-                getLog().info(element);
-            }
+            infoLog(element);
         }
-        if (showLogs) {
-            getLog().info("About to instantiate URLClassLoader object using urls and context class loader");
-        }
+        infoLog("About to instantiate URLClassLoader object using urls and context class loader");
         ClassLoader contextClassLoader = URLClassLoader.newInstance(urls.toArray(new URL[0]),
                 this.getClass().getClassLoader());
-        if (showLogs) {
-            getLog().info("Context Class Loader instantiated with " + contextClassLoader.toString());
-            getLog().info("About to set Thread context class loader using the URLClassLoader object");
-        }
+        infoLog("Context Class Loader instantiated with " + contextClassLoader.toString());
+        infoLog("About to set Thread context class loader using the URLClassLoader object");
         Thread.currentThread().setContextClassLoader(contextClassLoader);
-        if (showLogs) {
-            getLog().info("Context Class Loader set with " + contextClassLoader.toString());
-        }
+        infoLog("Context Class Loader set with " + contextClassLoader.toString());
         return contextClassLoader;
+    }
+
+    private void infoLog(String log) {
+        if (showLogs) {
+            getLog().info(log);
+        }
+    }
+
+    private void warnLog(String log) {
+        if (showLogs) {
+            getLog().warn(log);
+        }
+    }
+
+    private void errorLog(String log) {
+        if (showLogs) {
+            getLog().error(log);
+        }
     }
 }
